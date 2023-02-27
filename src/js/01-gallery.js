e25d3862ac0bb3d76cfad9db7627606c99dbf917
Damian Tchorzewski <damiantchorzewski0@gmail.com>
2023-02-27 02:42:59 +0100

    edit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from "../../node_modules/simplelightbox";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import "../../node_modules/simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+
+
+const images = galleryItems.map(item => `
+  <a href="${item.original}">
+    <img src="${item.preview}" alt="${item.description}" />
+  </a>
+`).join("");
+
+const gallery = new SimpleLightbox(images, {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+gallery.on('show.simplelightbox', function () {
+  console.log('Galeria pokazana');
+}).on('shown.simplelightbox', function () {
+  console.log('Galeria została pokazana');
+}).on('close.simplelightbox', function () {
+  console.log('Galeria zamknięta');
+}).on('closed.simplelightbox', function () {
+  console.log('Galeria została zamknięta');
+});
