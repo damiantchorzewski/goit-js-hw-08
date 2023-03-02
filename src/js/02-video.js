732c2db49b03cf3bb274ce93ca4726299f1eb126
Damian Tchorzewski <damiantchorzewski0@gmail.com>
2023-03-02 03:43:56 +0100

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+// Inicjalizacja odtwarzacza Vimeo
+import throttle from 'lodash.throttle';
+const player = new Vimeo.Player('vimeo-player');
 
+// Funkcja do aktualizacji czasu odtwarzania
+const updateCurrentTime = throttle(function() {
+  player.getCurrentTime().then(function(seconds) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  });
+}, 1000);
+
+// Obsługa zdarzenia timeupdate
+player.on('timeupdate', updateCurrentTime);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+// Ustawiamy czas odtwarzania na zapisany lub 0
+player.setCurrentTime(savedTime || 0);
